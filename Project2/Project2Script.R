0c160b094ef8539579d0f9c67426237698abb6df
Derek Samsom <dsamsom@me.com>
2017-12-26 13:51:30 -0800

    Updating Project2Script

--- Project2/Project2Script.R
+++ Project2/Project2Script.R
@@ -9,16 +9,18 @@ titanic_original <- read.csv("titanic_original.csv", header = TRUE)
 # 1: Port of embarkation
 titanic_clean <- titanic_original %>%
   mutate(
-    embarked = replace(.$embarked, .$embarked == "", "S"),
+    embarked = replace(embarked, embarked == "", "S"),
 
 # 2: Age
-    age = replace(age, is.na(age), age == mean(age))
+    age = replace(age, is.na(age), mean(age, na.rm = TRUE)),
 
 # 3: Lifeboat
+    boat = replace(boat, boat == "", NA),
 
-)
 # 4: Cabin
+    has_cabin_number = ifelse(cabin == "", 1, 0)
+)
 
-#
 # 5: Submit the project on Github
 
+write.csv(titanic_clean, file="titanic_clean.csv")
